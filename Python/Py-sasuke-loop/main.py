86e5fbe46a30bd6938c8aff3c378c20d34199666
GabrielDamian <damiangabriel54@yahoo.com>
2022-05-08 22:27:34 +0300

    Fixes Sasuke Loop

--- Python/Py-sasuke-loop/main.py
+++ Python/Py-sasuke-loop/main.py
@@ -4,18 +4,20 @@ from GraphHandler.graphHandler import *
 import time
 import matplotlib.pyplot as plt
 
-#TODO: check future price custom based on the current patterns
-#GLOBALS
+# TODO: check future price custom based on the current patterns
+# GLOBALS
 decisions = None
-tik_tok = 0.1 #clock intervals
+tik_tok = 0.1  # clock intervals
 time_check_old_decisions = 20
+offset_to_ignore_decisions = 20
 
 global_fake_api_next = []
 last_used = 0
 test_location = 'LastWeek5.csv'
 
-#temp declarations
-fake_current_time_stamp = 111  #magic number bcs you need offset
+# temp declarations
+fake_current_time_stamp = 111  # magic number bcs you need offset
+
 
 def readDataFromExcelFile(fileName):
     # print("Citesc date din fisier...")
@@ -50,20 +52,21 @@ def readDataFromExcelFile(fileName):
             float_arr.append(float(x))
         return float_arr
 
+
 def fakeApi(last_x):
     # test_location = 'LastWeek5.csv'
-    raw_data = readDataFromExcelFile('LastWeeks/'+test_location)
+    raw_data = readDataFromExcelFile('LastWeeks/' + test_location)
 
     add_time_stamp = []
     for index, a in enumerate(raw_data):
-        add_time_stamp.append([a,index])
+        add_time_stamp.append([a, index])
 
     # print("last x:",last_x)
     # print("fake_current_time_stamp:",fake_current_time_stamp)
     # print("from:",fake_current_time_stamp-last_x)
     # print("to:",fake_current_time_stamp)
 
-    temp_state = add_time_stamp[fake_current_time_stamp-last_x:fake_current_time_stamp ]
+    temp_state = add_time_stamp[fake_current_time_stamp - last_x:fake_current_time_stamp]
     # print("temp_state:",temp_state)
 
     just_y = []
@@ -71,15 +74,16 @@ def fakeApi(last_x):
         just_y.append(a[0])
     return just_y
 
+
 def plotArr(arr, id):
-    #arr, just y's
+    # arr, just y's
     plt.figure(id)
     plt.plot(arr)
 
-#end of temp declarations
 
-def integratedNarutoMain(points, candleSize,filter_candles_1,filter_candles_2):
+# end of temp declarations
 
+def integratedNarutoMain(points, candleSize, filter_candles_1, filter_candles_2):
     # print("Entry point graph handler")
     # print("[1].Raw data:\n",points)
     # print("len raw data:", len(points))
@@ -87,9 +91,9 @@ def integratedNarutoMain(points, candleSize,filter_candles_1,filter_candles_2):
     # plotArr(points, 'data')
     graph = graphData()
 
-    vector = points #wtf man
+    vector = points  # wtf man
 
-    #PLOT RAW DATA
+    # PLOT RAW DATA
     # plotArr(points,'ceva')
     graph.setInputData(vector)
 
@@ -97,7 +101,7 @@ def integratedNarutoMain(points, candleSize,filter_candles_1,filter_candles_2):
     # print("input to candle:",len(graph.candlesData))
 
     # print("[2].Input to candle:\n", graph.candlesData)
-    #PLOT INPUT TO CANDLE
+    # PLOT INPUT TO CANDLE
     # for a in graph.candlesData:
     #     plt.bar(a[0],a[1],0.5,a[2],color=a[3])
 
@@ -105,7 +109,7 @@ def integratedNarutoMain(points, candleSize,filter_candles_1,filter_candles_2):
 
     graph.filterCandles(filter_candles_1, filter_candles_2)
     # print("input to candle filtered:",len(graph.candlesData))
-    #PLOT FILTERED CANDLES
+    # PLOT FILTERED CANDLES
     # plt.title('altceva')
     # for a in graph.candlesData:
     #     plt.bar(a[0], a[1], 0.5, a[2], color=a[3])
@@ -130,7 +134,7 @@ def integratedNarutoMain(points, candleSize,filter_candles_1,filter_candles_2):
 
     # print("[5].Generate intern points:\n", graph.candlesToFunction)
 
-    #FINAL DATA IN graph.candlesToFunction
+    # FINAL DATA IN graph.candlesToFunction
     # for a in graph.candlesToFunction:
     #     print(a)
 
@@ -143,16 +147,17 @@ def integratedNarutoMain(points, candleSize,filter_candles_1,filter_candles_2):
         just_y.append(a[1])
 
     # plotArr(just_y,'ceva')
-    plt.show()
+    # plt.show()
 
     return just_y
+
 
 def readFromFile(fileName):
     file1 = open(fileName, 'r')
     lines = file1.readlines()
     patterns = []
     for a in lines:
-        pairs_str = a[0:len(a)-2].split("/")
+        pairs_str = a[0:len(a) - 2].split("/")
         single_pattern = []
 
         for b in pairs_str:
@@ -162,10 +167,10 @@ def readFromFile(fileName):
         patterns.append(single_pattern)
     return patterns
 
-def terraFormPatterns(referinteTerraForm, size_seg_unic, candleSize, filter_1, filter_2, abatere_hard):
-    #citeste pattern din fisier si adauga in structura template pentru patterns
-    patterns = readFromFile(referinteTerraForm)
 
+def terraFormPatterns(referinteTerraForm, size_seg_unic, candleSize, filter_1, filter_2, abatere_hard):
+    # citeste pattern din fisier si adauga in structura template pentru patterns
+    patterns = readFromFile(referinteTerraForm)
 
     obj = {}
 
@@ -180,12 +185,14 @@ def terraFormPatterns(referinteTerraForm, size_seg_unic, candleSize, filter_1, f
 
     return obj
 
+
 def findMinSizePattern(patterns):
     min = patterns[0]["size"]
     for a in patterns:
-        if(a["size"]<min):
+        if (a["size"] < min):
             min = a["size"]
     return min
+
 
 def initFormatDecisions(referinteTerraForm):
     final = {}
@@ -193,8 +200,9 @@ def initFormatDecisions(referinteTerraForm):
         final[a["fileName"]] = []
     return final
 
+
 def crossCorelation(pattern, lastSnapTerraFormed):
-    #yScallerEnaber and powerSum are default here
+    # yScallerEnaber and powerSum are default here
 
     # print("inside cros cor:")
     # print("patt:", pattern)
@@ -207,12 +215,11 @@ def crossCorelation(pattern, lastSnapTerraFormed):
     # print("max_y_base:",max_y_patt)
     # print("min spnap:",min(lastSnapTerraFormed))
 
-
-    #test
-    minus_min = [a-min(lastSnapTerraFormed) for a in lastSnapTerraFormed]
+    # test
+    minus_min = [a - min(lastSnapTerraFormed) for a in lastSnapTerraFormed]
 
     y_ratio = max_y_patt / max(minus_min)
-    test = [a*y_ratio for a in minus_min]
+    test = [a * y_ratio for a in minus_min]
 
     # print("ratio:", y_ratio)
 
@@ -222,67 +229,71 @@ def crossCorelation(pattern, lastSnapTerraFormed):
     # plotArr(pattern,'ceva')
     # plt.show()
 
-
     suma_scaled = 0
     for index, a in enumerate(pattern):
-        var = a - test[index]*y_ratio
-        suma_scaled += var*var
+        var = a - test[index] * y_ratio
+        suma_scaled += var * var
 
     return suma_scaled
 
-def completeOldDecisions(currentStockPrice,currentTime):
-    #TODO: function to fetch the current stock price
+
+def completeOldDecisions(currentStockPrice, currentTime):
+    # TODO: function to fetch the current stock price
     # currentStockPrice = None
 
     for a in decisions:
-        for index,b in enumerate(decisions[a]):
+        for index, b in enumerate(decisions[a]):
             # curentTimeStamp = round(time.time() * 1000)
             curentTimeStamp = currentTime
 
-            if b.getEndDate() == None and curentTimeStamp - b.getStartDate() > time_check_old_decisions :
+            if b.getEndDate() == None and curentTimeStamp - b.getStartDate() > time_check_old_decisions:
                 print("decision completed")
-                decisions[a][index].completeazaEndMomentum(curentTimeStamp,currentStockPrice)
+                decisions[a][index].completeazaEndMomentum(curentTimeStamp, currentStockPrice)
 
-    #TODO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    #overrwrite new decision into file (! not append)
+    # TODO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # overrwrite new decision into file (! not append)
+
 
 def printFinalPattern(final_patterns):
     for a in final_patterns:
         print("\n\nNew pattern:---------")
-        print("source:",a["source"])
-        print("size:",a["size"])
+        print("source:", a["source"])
+        print("size:", a["size"])
         # print(a["pytonTerraForm"])
         for x in a["pytonTerraForm"]:
-            print(x,":",a["pytonTerraForm"][x])
+            print(x, ":", a["pytonTerraForm"][x])
         print("values:")
-        for index,b in enumerate(a["values"]):
+        for index, b in enumerate(a["values"]):
             if index < 5:
                 print(b)
+
 
 def judgeDecisions(decisionsParam):
     total = len(decisionsParam)
     success = 0
     for a in decisionsParam:
         if a.priceDiff() > 0:
-            success +=1
-    print("Judge Decisions:",decisionsParam)
-    print("Total:",total)
-    print("Success:",success)
+            success += 1
+    print("Judge Decisions:", decisionsParam)
+    print("Total:", total)
+    print("Success:", success)
     if total and success:
-        print("Ratio:",(success*100)/total)
+        print("Ratio:", (success * 100) / total)
+
 
 class EntityDecizie:
-    def __init__(self,startDate,startPrice,endDate,endPrice,whichPatt,minCross):
-            self.start_date = startDate
-            self.start_price = startPrice
-            self.end_date = endDate
-            self.end_price = endPrice
-            self.which_one_pattern_var = whichPatt
-            self.min_cross_cor = minCross
+    def __init__(self, startDate, startPrice, endDate, endPrice, whichPatt, minCross):
+        self.start_date = startDate
+        self.start_price = startPrice
+        self.end_date = endDate
+        self.end_price = endPrice
+        self.which_one_pattern_var = whichPatt
+        self.min_cross_cor = minCross
 
     def print(self):
         print("todo print patt")
-    def completeazaEndMomentum(self,endDate,endPrice):
+
+    def completeazaEndMomentum(self, endDate, endPrice):
         self.end_date = endDate
         self.end_price = endPrice
 
@@ -297,22 +308,84 @@ class EntityDecizie:
             return self.end_price - self.start_price
         else:
             return -1
+
     def printMe(self):
-        print(self.start_price,self.end_price,self.start_date,self.end_date)
+        print(self.start_price, self.end_price, self.start_date, self.end_date)
+
+    def toFileFormat(self):
+        string = ""
+        # FORMAT"
+        # YES or NO = end_price - start_price > 0
+        # YES/start_price/end_price/start_date/end_date
+        if self.end_price is not None and self.start_price is not None and self.end_price - self.start_price > 0:
+            string += 'YES/'
+        else:
+            string += 'NO /'
+
+        string += str(self.start_price) + '/'
+        string += str(self.end_price) + '/'
+        string += str(self.start_date) + '/'
+        string += str(self.end_date) + '/'
+
+        return string
+
+    def areYouOk(self):
+        if self.start_price is None or self.end_price is None:
+            return None
+        else:
+            return self.end_price - self.start_price
 
 def testFunction():
-    raw_data = readDataFromExcelFile('LastWeeks/'+'LastWeek5.csv')
+    raw_data = readDataFromExcelFile('LastWeeks/' + 'LastWeek5.csv')
     print("raw data:", raw_data)
-    terr_raw_data = integratedNarutoMain(raw_data,1,0.3,0.5)
+    terr_raw_data = integratedNarutoMain(raw_data, 1, 0.3, 0.5)
     print("terra:", terr_raw_data)
 
-    plotArr(raw_data[0:200],'testFct')
-    plotArr(terr_raw_data[0:200],'testFct')
+    # plotArr(raw_data[0:200],'testFct')
+    plotArr(terr_raw_data[0:200], 'testFct')
     plt.show()
+
+def outputAtClock(newDecision, destination):
+    with open(destination, "a") as myfile:
+        myfile.write(newDecision.toFileFormat() +"\n")
+    # function that write into file when patterns where generated / append each time
+
+def parsePatternIntoFile(source, destination):
+    # write pattern into file format, overwrite each time
+
+    print("entry point parsePAtternIntoFile")
+    print(source)
+
+    string = ""
+    for a in source:
+        string += "\n"
+        string += a + "\n"
+        string += "Ratio:" + str(round(extractRatioFromPattern(source[a]),2)) + "\n"
+        string += "Total:" + str(len(source[a])) + "\n"
+
+        for b in source[a]:
+            string += b.toFileFormat() + "\n"
+
+    with open(destination, 'w') as f:
+        f.write(string)
+
+def extractRatioFromPattern(sourceArr):
+    total = 0
+    good = 0
+    for decision in sourceArr:
+        if decision.areYouOk() is not None:
+            total += 1
+            if decision.areYouOk() > 0:
+                good +=1
+
+    if total == 0:
+        return 0
+    else:
+        return (good*100)/total
 
 if __name__ == '__main__':
     # testFunction()
-    #------initializare patterns------
+    # ------initializare patterns------
     referinteTerraForm = [
         # {
         #     "fileName": "Patterns/pattern_55.txt",
@@ -346,6 +419,14 @@ if __name__ == '__main__':
             "filter_2": 0.5,
             "abatere_hard": 5000
         },
+        {
+            "fileName": "Patterns/pattern_6.txt",
+            "size_seg_unic": 30,
+            "candle_size": 2,
+            "filter_1": 0.3,
+            "filter_2": 0.5,
+            "abatere_hard": 2000
+        },
         # {
         #     "fileName": "Patterns/pattern_8.txt",
         #     "size_seg_unic": 25,
@@ -363,40 +444,47 @@ if __name__ == '__main__':
         #     "abatere_hard": 200
         # },
     ]
+
     patterns = []
     for a in referinteTerraForm:
-        patterns.append(terraFormPatterns(a["fileName"],a["size_seg_unic"],a["candle_size"],a["filter_1"],a["filter_2"],a["abatere_hard"]))
+        patterns.append(
+            terraFormPatterns(a["fileName"], a["size_seg_unic"], a["candle_size"], a["filter_1"], a["filter_2"],
+                              a["abatere_hard"]))
     decisions = initFormatDecisions(referinteTerraForm)
 
-    # test = BiananceMaster()
+    test = BiananceMaster()
 
     clock_time = 3
     while True:
-        print("clock:",clock_time)
+
+        parsePatternIntoFile(decisions, 'decisionsMars.txt')
+        # outputAtClock(decisions, 'decisionAtClock')
+
+        print("clock:", clock_time)
         clock_time += 1
         fake_current_time_stamp += 1
         if fake_current_time_stamp > 10950:
             break
 
-        last_100_min = fakeApi(70)   #just_y
+        last_100_min = fakeApi(70)  # just_y
         # print("last 100 min:", last_100_min)
 
         offset = 15
-        completeOldDecisions(last_100_min[-10],fake_current_time_stamp-offset)
+        completeOldDecisions(last_100_min[-10], fake_current_time_stamp - offset)
 
         for index, a in enumerate(patterns):
             candle_size = float(a["pytonTerraForm"]["candle_size"])
             filter_1 = float(a["pytonTerraForm"]["filter_1"])
             filter_2 = float(a["pytonTerraForm"]["filter_2"])
 
-            last_100_min_terraFormed = integratedNarutoMain(last_100_min,candle_size,filter_1,filter_2)
+            last_100_min_terraFormed = integratedNarutoMain(last_100_min, candle_size, filter_1, filter_2)
             # print("last 100 min terra:", last_100_min_terraFormed)
 
             size = a["size"]
-            start_index = len(last_100_min_terraFormed)-size-offset-1
+            start_index = len(last_100_min_terraFormed) - size - offset - 1
             end_index = start_index + size + 1
 
-            #print("start:end->",start_index,end_index)
+            # print("start:end->",start_index,end_index)
 
             last_x_points = last_100_min_terraFormed[start_index:end_index]
             # print("last x points:", last_x_points)
@@ -404,7 +492,7 @@ if __name__ == '__main__':
             min_cross_cor = crossCorelation(a["values"][0], last_x_points)
 
             which_one_pattern_var = None
-            for index_b,b in enumerate(a["values"]):
+            for index_b, b in enumerate(a["values"]):
 
                 if crossCorelation(b, last_x_points) < min_cross_cor:
                     min_cross_cor = crossCorelation(b, last_x_points)
@@ -421,9 +509,20 @@ if __name__ == '__main__':
                     which_one_pattern_var,
                     min_cross_cor
                 )
-                decisions[a["source"]].append(entitate_decizie)
+                print("TEST:")
+                if len(decisions[a["source"]]) > 0:
+                    print("ignore new decision")
+                    if  entitate_decizie.getStartDate() - decisions[a["source"]][-1].getStartDate() < offset_to_ignore_decisions:
+                        print("IGNORE")
+                        print("TEST 1:",decisions[a["source"]][-1].getStartDate())
+                        print("TEST 2:",entitate_decizie.getStartDate())
+                    else:
+                        decisions[a["source"]].append(entitate_decizie)
+                else:
+                    decisions[a["source"]].append(entitate_decizie)
+                    outputAtClock(entitate_decizie, "outputAtClock.txt")
 
-    print("decision:",len(decisions))
+    print("decision:", len(decisions))
     print(decisions)
 
     for a in decisions:
